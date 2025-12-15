f1cb7561abf76fe641d58bb13157dcabf8319c77
cyruslorenzo <cyruslorenzo0626@gmail.com>
2025-12-14 22:05:24 -0800

    Added Sound Effect For Drums

--- 4.DrumKit-Project/index.js
+++ 4.DrumKit-Project/index.js
@@ -8,10 +8,55 @@ var numOfButton = document.querySelectorAll(".drum").length
                                                 //That has class drum
     for (var i = 0; i <numOfButton; i++)
     {
+
     document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
-  }
+        
+    //identuty of button
+        
+    }
+
 
 function handleClick()
 {
-  alert("Got clicked!");
+    console.log(this.innerHTML);
+    var pressed = this.innerHTML;
+    // alert("Got clicked!");
+    // var audio = new Audio("./sounds/crash.mp3");
+    // audio.play();
+
+    switch (pressed) {
+        case "w":
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+            break;
+        case "a":
+         var audio = new Audio("./sounds/tom-2.mp3");
+        audio.play();
+            break;
+        case "s":
+         var audio = new Audio("./sounds/tom-3.mp3");
+        audio.play();
+            break;
+         case "d":
+         var audio = new Audio("./sounds/tom-4.mp3");
+        audio.play();
+            break;
+         case "j":
+         var audio = new Audio("./sounds/snare.mp3");
+        audio.play();
+            break;
+        case "k":
+         var audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+            break;
+         case "l":
+         var audio = new Audio("./sounds/kick-bass.mp3");
+         audio.play();
+            break;
+        default:
+            console.log(this.innerHTML);
+            break;
+    }
+
 }
+
