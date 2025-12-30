c81990d1a7ec8dd4f246c5b6b2841cc4554a7297
cyruslorenzo <cyruslorenzo0626@gmail.com>
2025-12-30 02:25:36 -0800

    Added Style in React JSX

--- 30.React/1.JSX-an-Babel/src/index.js
+++ 30.React/1.JSX-an-Babel/src/index.js
@@ -1,3 +1,5 @@
+//PT.1
+
 //Create a react app from scratch.
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
@@ -8,15 +10,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Angela";
+// const name = "Angela";
 
+
+// ReactDOM.render(
+// <div>
+//     <h1>{name} Favourite Foods</h1>
+//     <ul>
+//         <li>Bacon</li>
+//         <li>Jamon</li>
+//         <li>Noodles</li>
+//     </ul>
+// </div>, document.getElementById("root"));
+
+
+//PT.2
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+
+let myName = "Cyrus";
+const currentYear = new Date().getFullYear();
 
 ReactDOM.render(
 <div>
-    <h1>{name} Favourite Foods</h1>
-    <ul>
-        <li>Bacon</li>
-        <li>Jamon</li>
-        <li>Noodles</li>
-    </ul>
+    <h1 className ="heading">Created By: {myName}</h1>
+    <p>Copyright {currentYear}</p>
 </div>, document.getElementById("root"));
