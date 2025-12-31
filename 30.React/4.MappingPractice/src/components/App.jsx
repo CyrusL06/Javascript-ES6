e709d78b62d26b9353d01c3633fc65075105a092
cyruslorenzo <cyruslorenzo0626@gmail.com>
2025-12-30 17:53:07 -0800

    Finished practice with Mapping

--- 30.React/4.MappingPractice/src/components/App.jsx
+++ 30.React/4.MappingPractice/src/components/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+
+function createCard(i){
+ return <Card  
+    key={i.id}
+    emoji={i.emoji}
+    name={i.emoji}
+    meaning ={i.meaning}
+  />
+}
 
 function App() {
   return (
@@ -9,11 +20,19 @@ function App() {
       </h1> 
 
       <dl className="dictionary">
+       {emojipedia.map(createCard)}
+      {console.log(emojipedia)} 
 
+{/* <Card  
+ 
+    emoji="W"
+    name="W"
+    meaning ="W"
+  /> */}
+        {/* <Card/>
         <Card/>
         <Card/>
-        <Card/>
-        <Card/> 
+        <Card/>  */}
       </dl>
     </div>
   );
