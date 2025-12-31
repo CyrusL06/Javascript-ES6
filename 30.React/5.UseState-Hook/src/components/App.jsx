7172193f9c336bd50c57a0c7b2bebb2c5f31d197
cyruslorenzo <cyruslorenzo0626@gmail.com>
2025-12-31 04:24:23 -0800

    UseduseState to re-render the values

--- 30.React/5.UseState-Hook/src/components/App.jsx
+++ 30.React/5.UseState-Hook/src/components/App.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { createRoot } from "react-dom/client";
 
 var count = 0;
-  function increase(){
-    console.log("Clicked");
-    count++;
 
-  }
 function App() {
+
+    
+             //Value //Function
+      const [count, setCount] = useState(0);
+
+    function increase(){
+     setCount( count + 1)
+    }
+
+    function decrease(){
+      setCount(count -1)
+    }
+
   return <div className="container">
     <h1>{count}</h1>
     <button onClick={increase}>+</button>
+    <button onClick={decrease}>-</button>
+
   </div>;
 }
 
--- 30.React/5.UseState-Hook/src/index.js
+++ 30.React/5.UseState-Hook/src/index.js
@@ -2,21 +2,11 @@ import React from "react";
 import "../src/styles.css"
 
 import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
 
 var count = 0;
-var isPressed = false;
+var isPressed = true
 
-  function increase(){
-    console.log("Clicked");
-    count++;
-    root.render(<div className="container">
-			<h1>{count}</h1>
-			<button onClick={increase}>+</button>
-			</div>);
-  }
 const root = createRoot(document.getElementById("root"));
-root.render(<div className="container">
-    <h1>{count}</h1>
-    <button onClick={increase}>+</button>
-  </div>);
+root.render(<App />);
