b8ed522e202d14406ce54f6142575f31bd0ec883
cyruslorenzo <cyruslorenzo0626@gmail.com>
2025-12-30 19:40:01 -0800

    Explored ES6 features

--- 30.React/3.MappingDatae-ToCOmponents/src/index.js
+++ 30.React/3.MappingDatae-ToCOmponents/src/index.js
@@ -5,3 +5,37 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
+
+var numbers = [3, 56, 2, 48, 5];
+
+
+//Map -Create a new array by doing something with each item in an array.
+        //method that takes function to tell what to do
+
+    function double(x){
+    return x*2;
+}
+    numbers.map(double);
+
+//Filter - Create a new array by keeping the items that return true.
+//keep items that are true...
+const newNum = numbers.filter(function(num){return num >10});
+//56,48
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+//if we want to summ all of it
+
+var newNumber
+numbers.forEach(function(currentMumber){newNumber += currentMumber})
+
+    //in reduce
+
+var newRedNum = numbers.reduce(function(accumulator, currentMumber)
+                {return accumulator + currentMumber}
+                );
+
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
